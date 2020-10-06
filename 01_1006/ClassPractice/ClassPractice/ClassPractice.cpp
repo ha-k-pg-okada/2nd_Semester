761d61958529184d38c7df6b09ed7099e0386de6
MSI\Miokiti Jr <mioneko115@gmail.com>
2020-10-06 14:27:13 +0900

    リファクタリングその1 ・クラス名を変更 ・クラスの定義を別のcpp.hに移動 ・よく使うヘッダーファイルのインクルードをCommon.hに集約

--- 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
+++ 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -1,62 +1,19 @@
-﻿#include <stdlib.h>
-#include <stdio.h>
+﻿#include "Unit.h"
+#include "Common.h"
 
-class Fighter
-{
-public:
-
-	//コンストラクタ
-	Fighter();
-
-	//デストラクタ
-	~Fighter();
-
-public:
-
-	int m_hp = 0;
-	int m_str = 0;
-	int m_mag = 0;
-	int m_tec = 0;
-	int m_spd = 0;
-	int m_def = 0;
-	int m_luc = 0;
-	int m_mde = 0;
-	
-
-
-};
-
-
-
-Fighter::Fighter()
-{
-	 m_hp = 0;
-	 m_str = 0;
-	 m_mag = 0;
-	 m_tec = 0;
-	 m_spd = 0;
-	 m_def = 0;
-	 m_luc = 0;
-	 m_mde = 0;
-}
-
-Fighter::~Fighter()
-{
-
-}
 
 int main()
 {
-	Fighter g_Fighter;
+	Unit g_Unit;
 
-	printf("m_hp = %d\n",g_Fighter.m_hp);
-	printf("m_str = %d\n", g_Fighter.m_str);
-	printf("m_mag = %d\n", g_Fighter.m_mag);
-	printf("m_tec = %d\n", g_Fighter.m_tec);
-	printf("m_spd = %d\n", g_Fighter.m_spd);
-	printf("m_def = %d\n", g_Fighter.m_def);
-	printf("m_luc = %d\n", g_Fighter.m_luc);
-	printf("m_mde = %d\n", g_Fighter.m_mde);
+	printf("m_hp = %d\n", g_Unit.m_hp);
+	printf("m_str = %d\n", g_Unit.m_str);
+	printf("m_mag = %d\n", g_Unit.m_mag);
+	printf("m_tec = %d\n", g_Unit.m_tec);
+	printf("m_spd = %d\n", g_Unit.m_spd);
+	printf("m_def = %d\n", g_Unit.m_def);
+	printf("m_luc = %d\n", g_Unit.m_luc);
+	printf("m_mde = %d\n", g_Unit.m_mde);
 
 
 
---
+++ 01_1006/ClassPractice/ClassPractice/Unit.h
@@ -0,0 +1,33 @@
+
+#ifndef UNIT_H
+#define UNIT_H
+
+class Unit
+{
+public:
+
+	//コンストラクタ
+	Unit();
+
+	//デストラクタ
+	~Unit();
+
+public:
+
+	int m_hp = 0;
+	int m_str = 0;
+	int m_mag = 0;
+	int m_tec = 0;
+	int m_spd = 0;
+	int m_def = 0;
+	int m_luc = 0;
+	int m_mde = 0;
+
+
+
+};
+
+
+
+
+#endif // !UNIT_H
