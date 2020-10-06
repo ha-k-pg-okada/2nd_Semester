caa178454dd9236a8451c5e267d9351d727811db
MSI\Miokiti Jr <mioneko115@gmail.com>
2020-10-06 11:05:35 +0900

    クラスの作り方の復習用プロジェクトの作成。

---
+++ 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -0,0 +1,46 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+class Fighter
+{
+public:
+
+	//コンストラクタ
+	Fighter();
+
+	//デストラクタ
+	~Fighter();
+
+public:
+
+	int m_hp = 0;
+	int m_str = 0;
+	int m_mag = 0;
+	int m_tec = 0;
+	int m_spd = 0;
+	int m_def = 0;
+	int m_luc = 0;
+	int m_mde = 0;
+	
+
+
+};
+
+Fighter::Fighter()
+{
+
+}
+
+Fighter::~Fighter()
+{
+
+}
+
+int main()
+{
+
+
+
+	system("pause");
+	return 0;
+}
