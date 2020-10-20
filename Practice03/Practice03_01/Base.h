d368d5973f5a50e8e4860724e88482197750534a
MSI\Miokiti Jr <mioneko115@gmail.com>
2020-10-20 10:16:36 +0900

    当たり判定用に、幅と高さの変数と関数を追加。

--- Practice03/Practice03_01/Base.h
+++ Practice03/Practice03_01/Base.h
@@ -9,20 +9,32 @@ public:
 	virtual ~Base();
 
 public:
+	//“–‚½‚è”»’è
+	//ˆø”‚Ì‹éŒ`î•ñ‚ÆA©•ª‚ª“–‚½‚Á‚Ä‚¢‚é‚©”»’f
+	virtual bool CheckHit(int x1, int y1, int width, int height);
+
+public:
 	void SetHp(int hp);
 	void SetPos(float x, float y);
 	void SetMoveSpeed(float speed);
+	void SetSize(int  width, int height);
+	
 
 	int GetHp();
 	float GetPosX();
 	float GetPosY();
 	float GetMoveSpeed();
+	int GetWidth();
+	int GetHeight();
 
 private:
 	int m_Hp;
 	float m_PosX;
 	float m_PosY;
 	float m_MoveSpeed;
+
+	int m_Width;  //“–‚½‚è”»’è—pQ•
+	int m_Height; //“–‚½‚è”»’è—pQ‚‚³
 };
 
 #endif // !BASE_H
